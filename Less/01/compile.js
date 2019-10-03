9ac41db1e56c7280d1c1773bd07b0b3eac7e0136
tim <969728883@qq.com>
2019-10-03 09:42:48 +0800

    less node compile

---
+++ Less/01/compile.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+var less = require('less');
+
+fs.readFile(path.resolve(__dirname, 'libs/style.less'), function (err, data) {
+  console.log(data.toString())
+  less.render(data.toString(),
+    {
+      paths: ['.', '`.`/lib', path.resolve(__dirname, 'libs/')],  // Specify search paths for @import directives
+      filename: 'style.less', // Specify a filename, for better error messages
+      compress: true          // Minify CSS output
+    },
+    function (e, output) {
+      fs.writeFile(path.resolve(__dirname, 'libs/style.css'), output.css, function (err) {
+        console.log(err, data)
+      })
+    });
+})
+
