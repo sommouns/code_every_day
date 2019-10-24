bd2c059e57771b84b75bde3a825837af71b01fbd
tim <969728883@qq.com>
2019-10-24 12:29:20 +0800

    minHeap

---
+++ leetcode/minHeap.js
@@ -0,0 +1,82 @@
+class MinHeap {
+  constructor(data) {
+    this.data = []
+    this.init(data)
+  }
+
+  init(data) {
+    for (let key = 0; key < data.length; key++) {
+      this.insert(data[key])
+    }
+  }
+
+  insert(val) {
+    var curPosition = this.data.length
+    this.data[curPosition] = val
+    this.adjust_insert(curPosition)
+    return true
+  }
+
+  remove() {
+    if (!this.data.length) return null
+    var min = this.data[0]
+    if (this.data.length > 1) {
+      this.data[0] = this.data.pop()
+    } else {
+      this.data.pop()
+    }
+    this.adjust_remove()
+    return min
+  }
+
+  adjust_insert(curPosition) {
+    var parentIndex = (curPosition - 1) >> 1
+    while (curPosition > 0) {
+      if (this.data[curPosition] >= this.data[parentIndex]) {
+        break
+      } else {
+        [this.data[curPosition], this.data[parentIndex]] = [this.data[parentIndex], this.data[curPosition]]
+        curPosition = parentIndex
+        parentIndex = (curPosition - 1) >> 1
+      }
+    }
+  }
+
+  adjust_remove() {
+    if (!this.data[0]) {
+      return
+    }
+    var leftNodeIndex = 1
+    var rightNodeIndex = 2
+    var curPosition = this.data[rightNodeIndex]
+      && this.data[leftNodeIndex] > this.data[rightNodeIndex]
+      ? rightNodeIndex
+      : leftNodeIndex
+    var parentIndex = (curPosition - 1) >> 1
+    while (curPosition < this.data.length) {
+      if (this.data[curPosition] >= this.data[parentIndex]) {
+        break
+      } else {
+        [this.data[curPosition], this.data[parentIndex]] = [this.data[parentIndex], this.data[curPosition]]
+        leftNodeIndex = (curPosition + 1) * 2 - 1
+        rightNodeIndex = (curPosition + 1) * 2
+        curPosition = this.data[leftNodeIndex] < this.data[rightNodeIndex] ? leftNodeIndex : rightNodeIndex
+        parentIndex = (curPosition - 1) >> 1
+      }
+    }
+  }
+
+
+  print() {
+    console.log(this.data)
+  }
+}
+
+var heap = new MinHeap([5, 4, 3, 2, 1])
+console.log(heap.remove())
+console.log(heap.remove())
+console.log(heap.remove())
+console.log(heap.remove())
+console.log(heap.remove())
+console.log(heap.remove())
+console.log(heap.remove())
