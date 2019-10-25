d864f6cb5ac899e08739742e931f67ef526d4c1f
tim <969728883@qq.com>
2019-10-25 09:16:03 +0800

    min_num_transformed_by_array

--- leetcode/min_num_transformed_by_array.js
+++ leetcode/min_num_transformed_by_array.js
@@ -1,9 +1,15 @@
 function minNum(arr) {
   if (!arr.length) return false
   var strArr = arr.map(v => '' + v)
-  strArr.sort(function (a, b) {
-    return b - a
-  })
+  strArr.sort(Comparator)
+  function Comparator(a, b) {
+    var s1 = a + "" + b;
+    var s2 = b + "" + a;
+    for (var i = 0; i < s1.length; i++) {
+      return s1 - s2
+    }
+    return 1;
+  }
   return strArr.join('')
 }
-console.log(minNum([3, 32, 321]))
+console.log(minNum([1, 321, 13]))
